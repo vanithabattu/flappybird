e6fa27d748feb50628a8f5f5fa9a3b2cfa36e194
vanithabattu <vanithabattu610@gmail.com>
2025-10-23 12:08:24 +0200

    week5  update

--- src/flappybird.js
+++ src/flappybird.js
@@ -1,57 +1,117 @@
-//board
+// Board
 let board;
 let boardWidth = 360;
 let boardHeight = 640;
 let context;
 
-//bird
-let birdWidth = 34; //width/height ratio = 408/228 = 17/12
+// Bird
+let birdWidth = 34;
 let birdHeight = 24;
 let birdX = boardWidth / 8;
 let birdY = boardHeight / 2;
 let birdImg;
 
-//pipes
-let pipeWidth = 64; //width/height ratio = 384/3072 = 1/8
+let bird = {
+  x: birdX,
+  y: birdY,
+  width: birdWidth,
+  height: birdHeight
+};
+
+// Pipes
+let pipeArray = [];
+let pipeWidth = 64;
 let pipeHeight = 512;
 let pipeX = boardWidth;
-let pipeY = 0;
 
 let topPipeImg;
 let bottomPipeImg;
+
+// Physics
+let velocityX = -2;
+let velocityY = 0;
+let gravity = 0.4;
 
 window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
   board.width = boardWidth;
-  context = board.getContext("2d"); //used for drawing on the board
+  context = board.getContext("2d");
 
-  //draw flappy bird
-  context.fillStyle = "green";
-  context.fillRect(birdX, birdY, birdWidth, birdHeight);
-
-  //load images
   birdImg = new Image();
   birdImg.src = "./flappybird.png";
-  birdImg.onload = function () {
-    context.drawImage(birdImg, birdX, birdY, birdWidth, birdHeight);
-  };
 
   topPipeImg = new Image();
   topPipeImg.src = "./toppipe.png";
-  topPipeImg.onload = function () {
-    context.drawImage(topPipeImg, 0, 0, pipeWidth, pipeHeight);
-    context.drawImage(topPipeImg, 80, 0, pipeWidth, pipeHeight / 2);
-    context.drawImage(topPipeImg, 160, 0, pipeWidth, pipeHeight / 3);
-    context.drawImage(topPipeImg, 240, 0, pipeWidth, pipeHeight / 4);
-  };
 
   bottomPipeImg = new Image();
   bottomPipeImg.src = "./bottompipe.png";
-  bottomPipeImg.onload = function () {
-    context.drawImage(bottomPipeImg, 0, 565, pipeWidth, pipeHeight / 4);
-    context.drawImage(bottomPipeImg, 80, 425, pipeWidth, pipeHeight / 3);
-    context.drawImage(bottomPipeImg, 160, 320, pipeWidth, pipeHeight / 2);
-    context.drawImage(bottomPipeImg, 240, 215, pipeWidth, pipeHeight);
-  };
+
+  requestAnimationFrame(update);
+  setInterval(placePipes, 1500);
+  document.addEventListener("keydown", moveBird);
 };
+
+// Main game loop
+function update() {
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
+
+  // Gravity
+  velocityY += gravity;
+  bird.y = Math.max(bird.y + velocityY, 0);
+
+  // Prevent falling below ground
+  if (bird.y + bird.height >= board.height) {
+    bird.y = board.height - bird.height;
+    velocityY = 0;
+  }
+
+  // Draw bird
+  drawBird();
+
+  // Move and draw pipes
+  for (let i = 0; i < pipeArray.length; i++) {
+    let pipe = pipeArray[i];
+    pipe.x += velocityX;
+    context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+  }
+}
+
+// Draw bird
+function drawBird() {
+  context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+}
+
+// Create pipes
+function placePipes() {
+  let randomPipeY = -pipeHeight / 4 - Math.random() * (pipeHeight / 2);
+  let openingSpace = boardHeight / 4;
+
+  let topPipe = {
+    img: topPipeImg,
+    x: pipeX,
+    y: randomPipeY,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false
+  };
+  pipeArray.push(topPipe);
+
+  let bottomPipe = {
+    img: bottomPipeImg,
+    x: pipeX,
+    y: randomPipeY + pipeHeight + openingSpace,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false
+  };
+  pipeArray.push(bottomPipe);
+}
+
+// Jump controls
+function moveBird(e) {
+  if (e.code === "Space" || e.code === "ArrowUp" || e.code === "KeyX") {
+    velocityY = -6;
+  }
+}
